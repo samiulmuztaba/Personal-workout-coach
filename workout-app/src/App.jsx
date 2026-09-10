a77a24f7f5b0fd1809e85635d6f8bf214a512cea
Samiul Muztaba <samiulmuztaba101@gmail.com>
2026-09-10 16:07:11 +0600

    (fix) - wrong spelling

--- workout-app/src/App.jsx
+++ workout-app/src/App.jsx
@@ -417,7 +417,7 @@ function App() {
 
   const WORKOUT_DAYS = {
     "Weeks 1-4": ["Sunday", "Tuesday", "Thursday"],
-    "Weeks 5-8": ["Sunday", "Tuesday", "Thrusday"],
+    "Weeks 5-8": ["Sunday", "Tuesday", "Thursday"],
     "Weeks 9-12": ["Monday", "Wednesday", "Thursday", "Friday"],
   };
 
